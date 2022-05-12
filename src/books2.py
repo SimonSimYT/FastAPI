bb0418c00e680a7839b2bf3802082d0bc808d4e0
Simon Sim Yew Teck <simonsimyt@gmail.com>
2022-05-12 12:55:10 +0800

    Completed Book2.py
    Updated requirements.yaml

--- src/books2.py
+++ src/books2.py
@@ -1,12 +1,18 @@
+from optparse import Option
 from typing import Optional
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException, Request, status, Form, Header
 from pydantic import BaseModel, Field
 from uuid import UUID
+from starlette.responses import JSONResponse
+
+class NegativeNumberException(Exception):
+    def __init__(self, books_to_return):
+        self.books_to_return = books_to_return
 
 app = FastAPI()
 
-# Uses BaseModel to enforce type hinting
 class Book(BaseModel):
+    """ Uses BaseModel to enforce type hinting """
     id:UUID
     title:str = Field(min_length=1) # data validation 
     author:str = Field(min_length=1, max_length=100)
@@ -29,8 +35,31 @@ class Book(BaseModel):
 
 BOOKS = []
 
+class BookNoRating(BaseModel):
+    id:UUID
+    title:str = Field(min_length=1)
+    author:str
+    description:Optional[str] = Field(
+        title="Description of book",
+        max_length=100,
+        min_length=1)
+
+@app.exception_handler(NegativeNumberException)
+async def negative_number_exception_handler(
+    request:Request,
+    exception:NegativeNumberException):
+    return JSONResponse(
+        status_code=418,
+        content={
+            'message':f'Hey, why do you want {exception.books_to_return} books'
+        })
+
 @app.get("/")
 async def read_all_books(books_to_return:Optional[int]=None):
+
+    if books_to_return and books_to_return < 0:
+        raise NegativeNumberException(books_to_return=books_to_return)
+
     if len(BOOKS) < 1:
         create_books_no_api()
     if books_to_return and len(BOOKS) >= books_to_return>0:
@@ -48,7 +77,30 @@ async def read_book(book_id:UUID):
         if x.id ==book_id:
             return x
 
-@app.post("/")
+@app.get("/book/rating/{book_id}",response_model=BookNoRating)
+async def read_book_no_rating(book_id:UUID):
+    for x in BOOKS:
+        if x.id ==book_id:
+            return x
+
+@app.get('/header"')
+async def read_header(random_header: Optional[str] = Header(None)):
+    return {"Random-Header":random_header}
+
+@app.post("/books/login")
+async def book_login(username:str = Form(...), password:str = Form(...)):
+    return{"username":username,"password":password}
+
+@app.post("/books/login/")
+async def book_login(
+    book_index:int, 
+    username:Optional[str] = Header(None), 
+    password:Optional[str] = Header(None)):
+    if username == "FASTAPIUser" and password == "123456":
+        return BOOKS[book_index]
+    return "Invalid User"
+
+@app.post("/", status_code=status.HTTP_201_CREATED)
 async def create_book(book:Book):
     BOOKS.append(book)
     return book
@@ -70,6 +122,12 @@ async def delete_book(book_id:UUID):
         if x.id == book_id:
             del BOOKS[counter-1]
             return f'ID:{book_id} deleted'
+    raise raise_item_cannot_be_found()
+
+def raise_item_cannot_be_found():
+    return HTTPException(
+        status_code=404, detail="Book Not Found",
+        headers={"X-Header-Error":'Nothing to be seen at UUID'})
 
 def create_books_no_api():
     book_1 = Book(
